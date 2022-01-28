07961e3208e75299f0230e1d9688bd205a4e000a
iamleahb <leah.ball@gmail.com>
2022-01-28 00:34:43 -0600

    created responsive nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link as ReactRouterDomLink, useLocation } from "react-router-dom";
-import { lightRed, midOcean } from "../theme/Colors";
+import { lightOcean, lightRed, midOcean, midRed } from "../theme/Colors";
 import { Logo } from "../theme/Logo";
 
 const HeaderWrapper = styled.header`
@@ -18,16 +18,25 @@ const HeaderLogo = styled(Logo)`
 `;
 
 const Menu = styled.div`
-  /* grid-area: "nav"; */
-  display: flex;
-  position: relative;
-  width: initial;
-  border-bottom: none;
-  margin: auto 8% auto auto;
-  font-family: "open-sans";
-  background: none;
-  left: initial;
-  top: initial;
+  display: block;
+  position: absolute;
+  width: 100%;
+  top: 60px;
+  left: 0px;
+  padding: 0;
+  box-sizing: border-box;
+  border-bottom: 2px solid ${midOcean};
+
+  @media (min-width: 768px) {
+    display: flex;
+    position: relative;
+    width: initial;
+    border-bottom: none;
+    margin: auto 8% auto auto;
+    background: none;
+    left: initial;
+    top: initial;
+  }
 `;
 
 const Link = ({ isActive, children, ...props }) => {
@@ -35,14 +44,19 @@ const Link = ({ isActive, children, ...props }) => {
 };
 
 const StyledLink = styled(Link)`
-  padding: 2% 4%;
+  padding: 1% 6%;
   display: block;
   text-align: left;
   box-sizing: border-box;
-  margin: auto 0;
+  margin: 0 0;
   font-family: "Poppins";
   color: ${midOcean};
   font-weight: ${(p) => (p.isActive ? 600 : 300)};
+  background-color: ${(p) => (p.isActive ? "white" : "#EFF3FF")};
+
+  @media (min-width: 768px) {
+    background-color: ${lightRed};
+  }
 `;
 
 export function Header() {
